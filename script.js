caa696b4d85550f49ce1044b55568b0f2639455b
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-10-26 20:33:49 -0500

    Add State constructor, push instances to locations

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 var locations = []; // this array will hold your objects
 
 // object constructor
+function State(state, revenue, locationCount){
+  this.state = state;
+  this.revenue = revenue;
+  this.locationCount = locationCount;
+
+  this.averageRevenue = function (){
+    return Math.round((this.revenue / this.locationCount)*100)/100;
+  }
+}
 
 // object instances
+var illinois = new State("IL", 5000, 12);
+var minnesota = new State("MN", 300, 3);
+var nevada = new State("NV", 25000, 1);
 
 // push object instances to locations array
+locations.push(illinois);
+locations.push(minnesota);
+locations.push(nevada);
 
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
